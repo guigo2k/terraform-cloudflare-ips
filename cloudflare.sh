22c71d3200e7befcf3c5066c3c3d59964e36569a
guigo2k <guigo2k@guigo2k.com>
2019-11-21 14:41:06 +0400

    feat(main): update external script

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 get_ips_v4() {
-  for ip in $(curl -X GET https://www.cloudflare.com/ips-v4); do
-    echo -n "$ip,"
+  for ip in $(curl --silent -X GET https://www.cloudflare.com/ips-v4); do
+    printf "$ip,"
   done | sed 's/.$//'
 }
 
 get_ips_v6() {
-  for ip in $(curl -X GET https://www.cloudflare.com/ips-v6); do
-    echo -n "$ip,"
+  for ip in $(curl --silent -X GET https://www.cloudflare.com/ips-v6); do
+    printf "$ip,"
   done | sed 's/.$//'
 }
 
