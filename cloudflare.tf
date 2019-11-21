dcbb32d572694778e5c1dfc1867fcfe1a4572745
guigo2k <guigo2k@guigo2k.com>
2019-11-21 14:37:43 +0400

    feat(main): upgrade to TF 0.12

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,17 +1,16 @@
-
 data "external" "cloudflare" {
   program = ["sh", "${path.module}/cloudflare.sh"]
 }
 
 locals {
-  cloudflare_ips_v4 = "${split(",", data.external.cloudflare.result.ips_v4)}"
-  cloudflare_ips_v6 = "${split(",", data.external.cloudflare.result.ips_v6)}"
+  ips_v4 = split(",", data.external.cloudflare.result.ips_v4)
+  ips_v6 = split(",", data.external.cloudflare.result.ips_v6)
 }
 
 output "ips_v4" {
-  value = "${local.cloudflare_ips_v4}"
+  value = local.ips_v4
 }
 
 output "ips_v6" {
-  value = "${local.cloudflare_ips_v6}"
+  value = local.ips_v6
 }
